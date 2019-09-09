cbe3c052d4858cd7aa97d78029773ebcaf0aa589
Philippe Antoine <contact@catenacyber.fr>
2019-09-09 13:53:10 +0200

    More relevant limit

--- htp/htp_decompressors.c
+++ htp/htp_decompressors.c
@@ -417,7 +417,8 @@ htp_decompressor_t *htp_gzip_decompressor_create(htp_connp_t *connp, enum htp_co
     switch (format) {
         case HTP_COMPRESSION_LZMA:
 #ifdef HAVE_LIBLZMA
-            rc = lzma_alone_decoder(&drec->lzstrm, GZIP_BUF_SIZE /* memlimit */);
+            rc = lzma_alone_decoder(&drec->lzstrm, 0x1000000 /* memlimit */);
+
             if (rc != LZMA_OK) {
                 rc = Z_DATA_ERROR;
             } else {
