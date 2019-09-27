4f1db37c6593f9d4ed3987174af04ecc4464876c
Philippe Antoine <contact@catenacyber.fr>
2019-09-27 13:24:08 +0200

    fuzz: fuzzit integration
    Launched by Travis which builds and upload fuzzers to fuzzit platform
    Uses secret key

---
+++ test/fuzzit.sh
@@ -0,0 +1,23 @@
+[ -s ./test/fuzz_htp ] || exit 0
+
+if [ "$TRAVIS_EVENT_TYPE" = 'cron' ]; then
+    FUZZING_TYPE=fuzzing
+else
+    FUZZING_TYPE=local-regression
+fi
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    FUZZIT_BRANCH="${TRAVIS_BRANCH}"
+else
+    FUZZIT_BRANCH="PR-${TRAVIS_PULL_REQUEST}"
+fi
+
+FUZZIT_ARGS="--type ${FUZZING_TYPE} --branch ${FUZZIT_BRANCH} --revision ${TRAVIS_COMMIT}"
+if [ "$FUZZIT_API_KEY" = '' ]; then
+    echo "missing FUZZIT_API_KEY, running anyways"
+fi
+
+wget -q -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/latest/download/fuzzit_Linux_x86_64
+chmod +x fuzzit
+set -x
+./fuzzit create job ${FUZZIT_ARGS} fuzz-htp-${QA_FUZZIT} ./test/fuzz_htp
+set +x
