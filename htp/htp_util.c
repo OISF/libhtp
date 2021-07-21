488eaa74e2107d0a3b878b312db5e41dd632735d
Philippe Antoine <contact@catenacyber.fr>
2021-07-21 14:12:23 +0200

    host: ipv6 address is a valid host

--- htp/htp_util.c
+++ htp/htp_util.c
@@ -40,6 +40,16 @@
 
 #include "htp_private.h"
 
+//inet_pton
+#ifndef __linux__
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#elif _WIN32
+#include <ws2tcpip.h>
+#endif
+
 /**
  * Is character a linear white space character?
  *
@@ -2442,6 +2452,17 @@ int htp_validate_hostname(bstr *hostname) {
 
     if ((len == 0) || (len > 255)) return 0;
 
+    if (data[0] == '[') {
+        // only ipv6 possible
+        if (len < 2) {
+            return 0;
+        }
+        char dst[sizeof(struct in6_addr)];
+        char str[INET6_ADDRSTRLEN];
+        memcpy(str, data+1, len-2);
+        str[len-2] = 0;
+        return inet_pton(AF_INET6, str, dst);
+    }
     while (pos < len) {
         // Validate label characters.
         startpos = pos;
